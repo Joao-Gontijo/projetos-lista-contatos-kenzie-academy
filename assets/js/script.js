eba34f745d3bdad563750b9d3b8196f7f7641ca5
Joao-Gontijo <jvgontijo166@gmail.com>
2022-02-07 11:21:12 -0300

    rendering list on html

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ const inputNome = document.getElementById('nome');
 const inputEmail = document.getElementById('email');
 const inputTelefone = document.getElementById('telefone');
 
+const listaContatosContainer = document.querySelector('.secaoListaContatos_Lista');
 
 const listaContatosLocal = [];
 
@@ -21,7 +22,36 @@ function adicionarContato(){
 
     listaContatosLocal.push(novoContato);
     
-    console.log(listaContatosLocal);
+    renderizarLista();
 }
 
 btn.addEventListener('click', adicionarContato);
+
+function renderizarLista(){
+    listaContatosContainer.innerHTML = '';
+    for(let i = 0; i < listaContatosLocal.length; i++){
+        criarNaTela(listaContatosLocal[i]);
+    }
+}
+
+function criarNaTela(contato){
+    const li = document.createElement('li');
+    const button = document.createElement('button');
+    const h2 = document.createElement('h2');
+    const p = document.createElement('p');
+    const span = document.createElement('span');
+
+    button.id = "removerContato"
+
+    //editar conteúdo de um elemento
+    h2.innerText = contato.nome;
+    p.innerText = contato.email;
+    span.innerText = contato.telefone;
+    //insere um elemento dentro de outro
+    li.appendChild(button);
+    li.appendChild(h2);
+    li.appendChild(p);
+    li.appendChild(span);
+
+    listaContatosContainer.appendChild(li);
+}
