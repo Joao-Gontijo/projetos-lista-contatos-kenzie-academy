c7fbb1f2bdb5e9ee367cedc698e353be84aa3074
Joao-Gontijo <jvgontijo166@gmail.com>
2022-02-07 16:36:32 -0300

    verifying fields and removing itens

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,73 @@
-
-
 const btn = document.getElementById('btn');
 const inputNome = document.getElementById('nome');
 const inputEmail = document.getElementById('email');
 const inputTelefone = document.getElementById('telefone');
 
+const meuStorage = localStorage;
+
 const listaContatosContainer = document.querySelector('.secaoListaContatos_Lista');
+const nomeInvalido = document.querySelector('.inputNomeClass');
+const emailInvalido = document.querySelector('.inputEmailClass');
+const telefoneInvalido = document.querySelector('.inputTelefoneClass');
 
 const listaContatosLocal = [];
+let id = 0;
 
 function adicionarContato(){
+    nomeInvalido.innerHTML = '';
+    emailInvalido.innerHTML = '';
+    telefoneInvalido.innerHTML = '';
+    
     const valorNome = inputNome.value;
     const valorEmail = inputEmail.value;
     const valorTelefone = inputTelefone.value;
 
     const novoContato = {
+        id: id,
         nome: valorNome,
         email: valorEmail,
         telefone: valorTelefone
     }
 
-    listaContatosLocal.push(novoContato);
-    
-    renderizarLista();
+    const p = document.createElement('p');
+
+    if(novoContato.nome == "" ){
+        p.innerText = "Campo nome em branco!";
+        nomeInvalido.appendChild(p);
+    } else if(novoContato.email == ""){
+        p.innerText = "Campo email em branco!";
+        emailInvalido.appendChild(p);
+    } else if(novoContato.telefone == ""){
+        p.innerText = "Campo telefone em branco!";
+        telefoneInvalido.appendChild(p);
+    } else {
+        id++;
+        listaContatosLocal.push(novoContato);
+        
+        renderizarLista();
+
+        inputNome.value = '';
+        inputEmail.value = '';
+        inputTelefone.value = '';
+    }
+
 }
 
 btn.addEventListener('click', adicionarContato);
 
 function renderizarLista(){
     listaContatosContainer.innerHTML = '';
-    for(let i = 0; i < listaContatosLocal.length; i++){
-        criarNaTela(listaContatosLocal[i]);
+    
+    if(listaContatosLocal.length !== 0){
+        for(let i = 0; i < listaContatosLocal.length; i++){
+            criarNaTela(listaContatosLocal[i]);
+        }
+    } else {
+        const listaVazia = `<li>
+            <p>Não há contatos na sua lista!</p>
+        </li>`
+
+        listaContatosContainer.innerHTML = listaVazia;
     }
 }
 
@@ -42,7 +79,10 @@ function criarNaTela(contato){
     const span = document.createElement('span');
 
     button.id = "removerContato"
+    button.addEventListener('click', removerContato);
 
+    //passa o id do contato para o html, no caso, para a lista
+    li.dataset.id = contato.id;
     //editar conteúdo de um elemento
     h2.innerText = contato.nome;
     p.innerText = contato.email;
@@ -54,4 +94,24 @@ function criarNaTela(contato){
     li.appendChild(span);
 
     listaContatosContainer.appendChild(li);
+
 }
+
+
+function removerContato(evento){
+    const botaoClicado = evento.target;
+    const contatoClicado = botaoClicado.parentElement;
+    const idContatoCliado = contatoClicado.dataset.id;
+    //find procura na lista o primeiro objeto onde for exatamente igual a condição
+    const contatoRemovido = listaContatosLocal.find((contato) => 
+        contato.id == idContatoCliado);
+    //retorna a posição do contato na lista
+    const posicaoContatoRemovido = listaContatosLocal.indexOf(contatoRemovido);
+
+    //retira o elemento da lista
+    listaContatosLocal.splice(posicaoContatoRemovido, 1);
+
+    renderizarLista();
+}
+
+renderizarLista();
